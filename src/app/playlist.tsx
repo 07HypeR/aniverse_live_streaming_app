3539ffa841f1ce357014ca52edd42c2814de3fdc
Abhik Baidya <abhikbaidya7@gmail.com>
2024-12-17 17:47:11 +0530

    Playlist screen added

--- src/app/playlist.tsx
+++ src/app/playlist.tsx
@@ -1,10 +1,21 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { commonStyles } from "@/styles/commonStyles";
+import { useRoute } from "@react-navigation/native";
+import { Anime } from "@/service/animeStore";
+import PlaylistHeader from "@/components/playlist/PlaylistHeader";
+import VideoPlayer from "@/components/playlist/VideoPlayer";
+import Interactions from "@/components/playlist/Interactions";
 
 const Page = () => {
+  const route = useRoute() as any;
+  const item = route?.params as Anime;
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={commonStyles.containerBlack}>
+      <PlaylistHeader title={item.title} genre={item.genre} />
+      <VideoPlayer item={item} />
+      <Interactions item={item} />
     </View>
   );
 };
